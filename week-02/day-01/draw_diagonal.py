a13a4e2b6e7d733fc2c165e99944515c6fba1e94
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-12 10:47:44 +0200

    all

--- week-02/day-01/draw_diagonal.py
+++ week-02/day-01/draw_diagonal.py
@@ -10,12 +10,45 @@
 # %%%%%
 #
 # The square should have as many lines as the number was
-c=10
-h="%"
-s=" "
-for i in range (1,c+1):
-    if i==1 or i==c:
-        print(h*c)
-    
+atikezer [5:53 PM] 
+Diagonal:
 
-        
+
+[5:53] 
+lines = int(input("Number of lines: "))
+puffer = ""
+
+for k in range(0, lines):
+    for i in range(0, lines):
+        if k == 0 or k == lines - 1:
+            puffer += "%"
+        else:
+            puffer = "%"
+            for l in range(1, lines - 1):
+                if l == k:
+                    puffer += "%"
+                else:
+                    puffer += " "
+            puffer += "%"
+    print (puffer + "\n")
+    puffer = ""
+
+
+[5:54] 
+Pyramid:
+
+
+[5:54] 
+lines = int(input("Number of lines: "))
+stars = ""
+spaces = ""
+j = 1
+for i in range(0, lines):
+    for k in range(0, j):
+        stars += "*"    
+    for k in range(0, lines - i):
+        spaces += " "
+    print (spaces + stars + "\n")
+    stars = ""
+    spaces = ""
+    j += 2
