791cee01737afc1d9c722a93693e5b0b9a61384a
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-12 17:25:35 +0200

    firsts

---
+++ week-02/day-03/append_letter.py
@@ -0,0 +1,6 @@
+# Add "a" to every string in far
+
+far = ["kuty", "macsk", "kacs", "rók", "halacsk"]
+for i in (0,4):
+    far += "a"
+print(far)
---
+++ week-02/day-03/simple_replace.py
@@ -0,0 +1,6 @@
+
+# I would like to replace "dishwasher" with "galaxy" in this example
+# Please fix it for me!
+# Expected ouput: In a galaxy far far away
+example={"diswasher":"galaxy"}
+print(example["diswasher"])
