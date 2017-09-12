64adb81a762107a2250eac4717d9a30d8e5d2197
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-12 15:41:31 +0200

    all

--- week-02/day-02/diagonal_matrix.py
+++ week-02/day-02/diagonal_matrix.py
@@ -7,10 +7,16 @@
 #   0 0 0 1
 #
 # - Print this two dimensional list to the output
+
+
 i=0
-while (i < 5):
+j=0
+c = [[], [], [], []]    
+while (i < 4):
     a=[0, 0, 0, 0]
     a[i]= 1
+    c[i]=a
+    print(c[i])
     i += 1
-    print(a)
-    
+    #print(a)
+
--- week-02/day-02/sum_all.py
+++ week-02/day-02/sum_all.py
@@ -3,6 +3,7 @@
 # - Print the sum of the elements in `ai`
 
 ai = [3, 4, 5, 6, 7]
-for i in range(1:5):
-    sum= ai[i]+ai[i+1]
-    print(sum)
+sum=0
+for n in ai:
+    sum +=n
+print(sum)
