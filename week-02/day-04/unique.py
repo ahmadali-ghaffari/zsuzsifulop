15ba63bb06af5cb8bd51cea3b93a1e7c07c121db
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-14 17:35:32 +0200

    feat(unique.py): fixed

--- week-02/day-04/unique.py
+++ week-02/day-04/unique.py
@@ -1,15 +1,15 @@
 # az inputból szedje ki az ismétlődő elemeket
 #output = [1, 11, 34, 52, 61]
-def only_once(numbers1, numbers2, numbers3, numbers4, numbers5,):
-    list = [numbers1, numbers2, numbers3, numbers4, numbers5]
+list_of_numbers = [0, 5, 4, 3, 2, 1]
+def only_once(list_of_numbers):
     new_list = []
-    for i in range(len(list)):
+    for i in range(len(list_of_numbers)):
         flag = True
         for j in range(len(new_list)):
-            if list[i] == new_list[j]:
+            if list_of_numbers[i] == new_list[j]:
                 flag=False
         if flag == True:
-            new_list.append(list[i])
+            new_list.append(list_of_numbers[i])
     print(new_list)
 
-only_once(1, 2, 3, 5, 1)
+only_once(list_of_numbers)
