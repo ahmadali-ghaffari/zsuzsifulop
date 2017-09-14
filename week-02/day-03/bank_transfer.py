b9724314e0b7a67ebae5bc1784eb25c58a58f82b
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-14 22:29:06 +0200

    feat(bank_transfer.py): fixed

--- week-02/day-03/bank_transfer.py
+++ week-02/day-03/bank_transfer.py
@@ -1,14 +1,45 @@
 accounts = [
-	{ 'client_name': 'Igor', 'account_number': 11234543, 'balance': 203004099.2 },
-	{ 'client_name': 'Vladimir', 'account_number': 43546731, 'balance': 5204100071.23 },
-	{ 'client_name': 'Sergei', 'account_number': 23456311, 'balance': 1353600.0 }
+	{ 'client_name': 'Igor', 'account_number': 111, 'balance': 10.5 },
+	{ 'client_name': 'Vladimir', 'account_number': 222, 'balance': 20.5},
+	{ 'client_name': 'Sergei', 'account_number': 333, 'balance': 30 }
 ]
 
 # Create function that returns the name and balance of cash on an account
-# def balance_client(accounts):
-#     for i in range(len(accounts)):
-#         print (accounts[i]['client_name'] +" " + str(accounts[i]['balance']))
-# balance_client(accounts)
+account_from = int(input("Give me the bank account number to transfer!"))
+account_to = int(input("Give me the bank account number to recive!"))
+amount_to_transfer = float(input("Give me the amount!"))
+def banking ():
+    something_goes_wrong = False
+    if accounts[0]['account_number'] != account_from and accounts[1]['account_number'] != account_from and accounts[2]['account_number'] != account_from:
+        print("404 - account not found for the transfer (account_from) ")
+        something_goes_wrong = True
+    elif accounts[0]['account_number'] != account_to and accounts[1]['account_number'] != account_to and accounts[2]['account_number'] != account_to:
+        print("404 - account not found for the transfer (account_to) ")
+        something_goes_wrong = True
+    elif something_goes_wrong == False:
+        for i in range(len(accounts)):
+            
+            if accounts[i]['account_number'] == account_from:
+                if accounts[i]['balance'] >= amount_to_transfer:
+                    new_balance = accounts[i]['balance'] - amount_to_transfer
+                    print(new_balance)
+            
+        for j in range(len(accounts)):
+            flag2 = False
+            if accounts[j]['account_number'] == account_to:
+                new_balance_2 = accounts[j]['balance'] + amount_to_transfer
+                print(new_balance_2)
+     
+       
+        
+            
+banking()
+
+
+
+
+
+
 
 
 # Create function that transfers an amount of cash from one account to another
@@ -18,33 +49,3 @@ accounts = [
 #  - amount to transfer
 #
 # Print "404 - account not found" if any of the account numbers don't exist
-
-#from=f
-#to=t
-#amount=a
-f = 11234543
-t = 43546731
-a = 203004099.2
-
-#ez a függvény megtalálja az f változóban tárolt számlát és levonja róla a összeget amit utalni szeretnénk
-
-def f_from(f, t, a):
-    for i in range(len(accounts)):
-        if (accounts[i]['account_number']) == f:
-            rest_of_money = accounts[i]['balance'] - a
-            print("Hey, I found the account", accounts[i]['account_number'], 'the old balance was', accounts[i]['balance'], 'the new balance is:', rest_of_money)
-    if (accounts[i]['account_number']) != f:
-        print("404 - account not found")
-        return
-        
-f_from(f, t, a)
-
-def t_to(f, t, a):
-    for i in range(len(accounts)):
-        if (accounts[i]['account_number']) == t:
-            new_money = accounts[i]['balance'] + a
-            print("Hey, I found the account", accounts[i]['account_number'], 'the old balance was', accounts[i]['balance'], 'the new balance is:', new_money)
-    if (accounts[i]['account_number']) != t:
-        print("404! - account not found")
-        return
-t_to(f, t, a)
