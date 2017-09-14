ae57a3c2fb4206799683c41d8ae4e70509daabf6
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-14 17:14:59 +0200

    feat(bubble.py): fixed

--- week-02/day-04/bubble.py
+++ week-02/day-04/bubble.py
@@ -1,19 +1,20 @@
 #buborékrendezés novekvő/illetve csökkenő módban 
-input = [34, 12, 24, 9, 5]
+list = [34, 12, 24, 9, 5]
 #output = [5, 9, 12, 24, 34]
 
-gate=True
-if gate == True:
+def sorted(z):           
+    if z == "asc":
+        for i in range(len(list)-1):
+            for j in range(i+1, len(list)):
+                if list[i] > list[j]:
+                    list[i], list[j] = list[j], list[i]
+        print(list)
 
-    for i in range(len(input)-1):
-        for j in range(i+1, len(input)):
-            if input[i] > input[j]:
-                input[i], input[j] = input[j], input[i]
-    print(input)
+    if z == "dec":
+        for i in range(len(list)-1):
+            for j in range(i+1, len(list)):
+                if list[i] < list[j]:
+                    list[i], list[j] = list[j], list[i]
+        print(list)
 
-if gate == False:
-    for i in range(len(input)-1):
-        for j in range(i+1, len(input)):
-            if input[i] < input[j]:
-                input[i], input[j] = input[j], input[i]
-    print(input)
+sorted("asc")
