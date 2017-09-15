5815863bf6e595d308b35fc6cd7492027acc16d6
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-15 15:02:29 +0200

    feat(guess_my_number): fixed

--- week-02/day-05/guess_my_number.py
+++ week-02/day-05/guess_my_number.py
@@ -1,21 +1,26 @@
 import random
-cycle = 5
+cycle_global = int(input("How many cycle would you like? "))
+from_which_number_global = int(input("Give me the first number of the range where I can guess my number! "))
+until_which_number_global =int(input("Give me the last number of the range where I can guess my number! "))
 
-the_number = random.randint(1,100)
-print("I thought about a number between 1 and 100, can you guess it in 5 rounds? :)")
-print(the_number)
-for i in range(cycle):
-    guessed_number = int(input("Give me a number! "))
-    if guessed_number > the_number:
-        print("Too high. You have " + str(cycle-i-1) +" lives left" )
+def rounds_and_range(cycle, from_which_number, until_which_number):
+    
+    the_number = random.randint(from_which_number, until_which_number)
+    print("I thought about a number between " + str(from_which_number) + " and " + str(until_which_number) + " ,can you guess it in " + str(cycle) + " steps?")
+    print("It is a secret, but my number is: " + str(the_number))
+    
+    for i in range(cycle):
+        guessed_number = int(input("Give me a number! "))
+        if guessed_number > the_number:
+            print("Too high. You have " + str(cycle-i-1) +" lives left" )
+            
+        elif guessed_number < the_number:
+            print("Too low. You have " + str(cycle-i-1) +" lives left.")
         
-    elif guessed_number < the_number:
-        print("Too low. You have " + str(cycle-i-1) +" lives left.")
-    
-    elif guessed_number == the_number:
-        print("Congratulations. You won!")
-        break
-    
-    if cycle-i-1 == 0:
-        print("You are not so good in this game, are you?")
-    
+        elif guessed_number == the_number:
+            print("Congratulations. You won!")
+            break
+        
+        if cycle-i-1 == 0:
+            print("You are not so good in this game, are you?")
+rounds_and_range(cycle_global, from_which_number_global, until_which_number_global)   
