7a59f43e4d0d852e8c80cfde7ff7ad4eefce75cc
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-15 11:38:08 +0200

    feat(palindrome_biulder.py): complete

---
+++ week-02/day-05/palindrome_builder.py
@@ -0,0 +1,11 @@
+text_global = "greenfox"
+
+def palindrome_builder(text):
+    backwards = []
+    for i in range(len(text)-1, -1, -1):
+        backwards += [text[i]]
+    backwards_join = "".join(backwards)
+    new_text=text+backwards_join
+    print(new_text)
+
+palindrome_builder(text_global)
