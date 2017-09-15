d8aaaae0db1b04c1cc122b174d92b0f76f989987
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-15 13:45:26 +0200

    feat(guess_my_number.py): complete

--- week-02/day-05/guess_my_number.py
+++ week-02/day-05/guess_my_number.py
@@ -1,19 +1,21 @@
 import random
-cycle = 6
-cycle_back = cycle-1
+cycle = 5
 
 the_number = random.randint(1,100)
+print("I thought about a number between 1 and 100, can you guess it in 5 rounds? :)")
 print(the_number)
-
-for i in range(cycle_back):
-    guessed_number = int(input("Give me a number!"))
+for i in range(cycle):
+    guessed_number = int(input("Give me a number! "))
     if guessed_number > the_number:
-        print("Too high. You have " + str(cycle_back-i-1) +" lives left" )
+        print("Too high. You have " + str(cycle-i-1) +" lives left" )
         
     elif guessed_number < the_number:
-        print("Too low. You have " + str(cycle_back-i-1) +" lives left.")
-        
+        print("Too low. You have " + str(cycle-i-1) +" lives left.")
+    
     elif guessed_number == the_number:
         print("Congratulations. You won!")
         break
+    
+    if cycle-i-1 == 0:
+        print("You are not so good in this game, are you?")
     
