2165c232a0bb8b9b18c4813378f5f434e253ce79
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-15 15:19:05 +0200

    feat(subint.py): complete

---
+++ week-02/day-04/subint.py
@@ -0,0 +1,22 @@
+numbers_original= [1, 11, 34, 52, 61]
+find_me_global = 1
+
+def find_me_somewhere(numbers, find_me):
+    
+    new_list = []
+    
+    for i in range(len(numbers)):
+        new_list.append(str(numbers[i]))
+
+    index_list = []
+    
+    for i in range(len(new_list)):
+        flag = True
+        for j in range(len(new_list[i])):
+            if new_list[i][j] == str(find_me) and flag:
+                index_list.append(i)
+                flag = False
+    print(index_list)
+
+
+find_me_somewhere(numbers_original, find_me_global)
