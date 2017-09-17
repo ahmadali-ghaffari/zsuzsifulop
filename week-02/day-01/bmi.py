d87a46ddd2443b7f1db3be48131d3bd773270e53
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-17 22:27:46 +0200

    feat(bmi.py): fixed

--- week-02/day-01/bmi.py
+++ week-02/day-01/bmi.py
@@ -1,7 +1,7 @@
 
-massInKg = 81.2
-heightInM = 1.78
+massInKg = 67.0
+heightInM = 1.7
 
 #Print the Body mass index (BMI) based on these values
-BMI=(heightInM ** 2)/massInKg
+BMI=massInKg/(heightInM **2)
 print(BMI)
