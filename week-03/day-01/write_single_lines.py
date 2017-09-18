dc15eecbe1f19a6553ca8ff14a91d951f7544936
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-18 12:17:03 +0200

    feat(write_single_lines.py): complete

---
+++ week-03/day-01/write_single_lines.py
@@ -0,0 +1,17 @@
+# Open a file called "my-file.txt"
+# Write your name in it as a text
+# If the program is unable to write the file,
+# then it should print an error message like: "Unable to write file: my-file.txt"
+
+the_file_source = "my-file.txt"
+
+def write_my_name(source):
+    try:
+        file = open(source, "w")
+        file.write("Hey, I am Zsuzsi")
+        
+    except FileNotFoundError:
+        print("Unable to write this file " + the_file_source)
+
+
+write_my_name(the_file_source)
