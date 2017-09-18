ea4ceae0c7f78e9b7fc7192dd4ef07f9d06d6b47
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-18 11:30:55 +0200

    feat(divide_by_zero.py): complete

---
+++ week-03/day-01/divide_by_zero.py
@@ -0,0 +1,14 @@
+# create a function that takes a number,
+# divides ten with it,
+# and prints the result.
+# it should print "fail" if the parameter is 0
+
+number_global = 0
+
+def divide(number):
+    try:
+        a = 10 / number  
+        print(a)
+    except ZeroDivisionError:
+        print("Hey, it is 0!")
+divide(number_global)
