5849a6cc6f4eac92a5f8e57943c1c7aa3ae29b80
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-18 14:28:47 +0200

    feat(doubled.py): complete

---
+++ week-03/day-01/doubled.py
@@ -0,0 +1,17 @@
+# Create a method that decrypts the duplicated-chars.txt
+file_name_global = "duplicated-chars.txt"
+def decrypt(file_name):
+    try:
+        file = open(file_name, "r")
+        text = ""
+        decrypt_copy_inside = open("decrypt_copy_inside.txt", "w")
+        lines_list = file.readlines()
+        for line in lines_list:
+            for i in range(0, len(line), 2):
+                text += line[i]
+        decrypt_copy_inside.write(text)
+        file.close()
+        decrypt_copy_inside.close()
+    except IOError:
+        print("Unable to write file: single-chars.txt")
+decrypt(file_name_global)
