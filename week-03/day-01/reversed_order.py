714236010dc9abdfa4a0b639b7aff9ac43a129f4
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-18 15:52:38 +0200

    feat(reversed_order.py): complete

---
+++ week-03/day-01/reversed_order.py
@@ -0,0 +1,16 @@
+# Create a method that decrypts reversed-order.txt
+file_name_global = "reversed-order.txt"
+def reversed_order(file_name):
+    try:
+        read_in = open(file_name_global, 'r')
+        text = read_in.readlines()
+        print(text)
+        string=""
+        for i in range(len(text)-1, -1, -1):
+            string += text[i]
+        print(string)
+    except IOError:
+        print("Unable to write file: single-chars.txt")
+
+reversed_order(file_name_global)
+        
