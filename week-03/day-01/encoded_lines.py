3eecbefe354cd4355da888fff135698b249df582
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-18 17:04:03 +0200

    feat(encoded_lines.py): complete

---
+++ week-03/day-01/encoded_lines.py
@@ -0,0 +1,25 @@
+# Create a method that decrypts encoded-lines.txt
+file_name_global = 'encoded_lines.txt'
+
+def shift_text_to_right(file_name):
+    read_in = open(file_name, 'r')
+    write_in = open('encoded_lines_copy_inside.txt', 'w')
+    text = read_in.readlines()
+    print(text)
+    string = ""
+    for line in text:
+        for j in range(len(line)):
+            if line[j] !=" ":
+                a = (ord(line[j])-1)
+                string += chr(a)
+            else:
+                string +=" "
+        string += "\n"
+    print(string)    
+            
+    write_in.write(string)
+
+
+
+
+shift_text_to_right(file_name_global)
