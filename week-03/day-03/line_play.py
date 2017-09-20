e9cd73a7b011f406d18d8eddb972f521b6eb588e
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-20 21:54:12 +0200

    feat(line_play.py): an easier and more understanable solution

--- week-03/day-03/line_play.py
+++ week-03/day-03/line_play.py
@@ -7,24 +7,17 @@ canvas.pack()
 
 # reproduce this:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
-
-def line_play_green(a, b):
-    c = 300-a
-    line = canvas.create_line(a, b, b, c, fill="green")
-line_play_green(0, 20)
-
-def line_play_red(a, b):
-    c = 300-a
-    line = canvas.create_line(b, a, c, b, fill="red")
-line_play_red(0, 20)
+def line_play(a, b, c, d, color):
+    line = canvas.create_line(a, b, c, d, fill=color)
 
 i = 0
 while i < 300:
+    line_play(0, i, i, 300, "green")
     i += 20
-    line_play_green(0, i)
-j = 0
-while j < 300:
-    j += 20
-    line_play_red(0,j)
+
+i = 0
+while i < 300:
+    line_play(300 - i, 0, 300, 300 - i, "pink")
+    i += 20
 
 root.mainloop()
