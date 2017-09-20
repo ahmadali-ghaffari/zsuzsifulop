545554917842d3c52de14f69673819ae6a51c8ee
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-20 11:32:42 +0200

    feat(colored_box.py): complete

---
+++ week-03/day-03/colored_box.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# draw a box that has different colored lines on each edge.
+left_line = canvas.create_line(10, 10, 10, 300, fill= "green")
+under_line = canvas.create_line(10, 300, 300, 300, fill ="orange")
+right_line = canvas.create_line(300, 10, 300, 300, fill="blue")
+top_line = canvas.create_line(10, 10, 300, 10, fill="grey")
+root.mainloop()
