8914088cf569d4d93adf5ed0e349ac04fae093de
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-20 18:22:43 +0200

    feat(starry_night.py): almost complete, without the random grey colour

---
+++ week-03/day-03/starry_night.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import random
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# draw the night sky:
+# - The background should be black
+# - The stars should be small squares
+# - The stars should have random positions on the canvas
+# - The stars should have random color (some shade of grey)
+background = canvas.create_rectangle(0, 0, 300, 300, fill="black")
+for i in range(0, 20):
+    x_top = random.randint(0, 300)
+    y_top = random.randint(0, 300)
+    rgb = random.randint(0, 255)
+    stars = canvas.create_rectangle(x_top, y_top, x_top+10, y_top+10, fill = "grey")
+root.mainloop()
