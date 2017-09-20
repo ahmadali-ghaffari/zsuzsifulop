a5a44036b4f71103abfae92815d9012257ee64b6
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-20 14:51:02 +0200

    feat(checkerboard.py): complete

---
+++ week-03/day-03/checkerboard.py
@@ -0,0 +1,43 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# fill the canvas with a checkerboard pattern.
+def checkerboard():
+    size = 35
+    x_top = 0
+    x_botton = x_top + size
+    y_top = 0
+    y_botton = 300/8
+    for j in range(0, 300):
+        for i in range(0, 300):
+            if j % 2 ==0:
+                if i % 2 == 0:
+                    square = canvas.create_rectangle(x_top, y_top, x_botton, y_botton, fill = "black")
+                    x_top += size
+                    x_botton += size
+                
+                else:
+                    square = canvas.create_rectangle(x_top, y_top, x_botton, y_botton, fill = "white")
+                    x_top += size
+                    x_botton += size
+            else:
+                if i % 2 == 0:
+                    square = canvas.create_rectangle(x_top, y_top, x_botton, y_botton, fill = "white")
+                    x_top += size
+                    x_botton += size
+                
+                else:
+                    square = canvas.create_rectangle(x_top, y_top, x_botton, y_botton, fill = "black")
+                    x_top += size
+                    x_botton += size
+        x_top = 0
+        x_botton = x_top + size
+        y_top += size
+        y_botton += size
+checkerboard()
+
+root.mainloop()
---
+++ week-03/day-03/purple_steps_3D.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# reproduce this:
+# [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
+
+#purple_square(10,10,25,25)
+def purple_cubics():
+    x_coordinate = 0
+    y_coordinate = 0
+    size = 15
+    for i in range(1, 20):
+        square = canvas.create_rectangle(x_coordinate, y_coordinate, x_coordinate + size, y_coordinate + size, fill = "red")
+        x_coordinate = x_coordinate + size
+        y_coordinate = y_coordinate + size
+        size += 10
+
+
+
+purple_cubics()
+root.mainloop()
