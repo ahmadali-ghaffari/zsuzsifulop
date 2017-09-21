4766634b379cc8f37d899eecdf8278ec53dc4e3e
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-21 12:03:06 +0200

    feat(string1.py): new integer names

--- week-03/day-04/string1.py
+++ week-03/day-04/string1.py
@@ -1,13 +1,13 @@
 # Given a string, compute recursively (no loops) a new string where all the
 # lowercase 'x' chars have been changed to 'y' chars.
 
-def string_transformer(word):
+def x_tansformer_to_y(word):
     if word == '':
         return ''
     else:
         if word[0] == "x":
-            return "y" + string_transformer(word[1:])
+            return "y" + x_tansformer_to_y(word[1:])
         else:
-            return word[0] + string_transformer(word[1:])
+            return word[0] + x_tansformer_to_y(word[1:])
        
-print(string_transformer("xalma"))
+print(x_tansformer_to_y("xalma"))
