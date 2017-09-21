73f9f31e63aca6f1843dc394d6d608fcdb372b3e
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-21 10:15:01 +0200

    feat(counter.py): integer names changes

--- week-03/day-04/counter.py
+++ week-03/day-04/counter.py
@@ -1,10 +1,10 @@
 # Write a recursive function that takes one parameter: n and counts down from n.
 
-def count_down(number):
-    if number == 0:
+def count_down(n):
+    if n == 0:
         return 0
     else:
-        print(number)
-        return count_down(number-1)
+        print(n)
+        return count_down(n-1)
 
-count_down(10)
+count_down(20)
