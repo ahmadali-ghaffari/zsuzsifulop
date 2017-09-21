204ddb71bd6cfcb483e657289f5fc5b496beb8b1
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-21 12:21:15 +0200

    feat(string.py): fixed

--- week-03/day-04/string3.py
+++ week-03/day-04/string3.py
@@ -4,6 +4,9 @@ def new_caracter_between_the_letters(word):
     if word == '':
         return ''
     else:
-        return word[0] + "*" + new_caracter_between_the_letters(word[1:])
+        if len(word) == 1:
+            return word[0] + new_caracter_between_the_letters(word[1:])
+        else:
+            return word[0] + "*" + new_caracter_between_the_letters(word[1:])
        
 print(new_caracter_between_the_letters("alma"))
