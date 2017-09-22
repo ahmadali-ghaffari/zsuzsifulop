6165a96f53d08c0fccbefff6bfcfd723151b0657
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-22 11:08:59 +0200

    feat(sierpinski_carpet.py): complete

---
+++ week-03/day-05/sierpinski_carpet.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+root = Tk()
+root.configure(background ='black')
+canvas = Canvas(root, width='600', height='600')
+canvas.pack()
+maximum_width = 600
+size = maximum_width / 3
+
+def mother_square(x_starting_point, y_starting_point, size):
+    square = canvas.create_rectangle(x_starting_point, y_starting_point, x_starting_point + size, y_starting_point + size, fill = "black")
+
+
+def sierpinski_carpet(x, y, size):
+    if size < 2:
+        return 
+    else:
+        mother_square(x, y, size)
+        size /= 3
+        sierpinski_carpet(x - 2 * size , y + size, size)
+        sierpinski_carpet(x + size , y - 2 * size, size)
+        sierpinski_carpet(x + 4 * size , y + size, size)
+        sierpinski_carpet(x + size , y + 4 * size, size)
+
+        sierpinski_carpet(x + 4 * size , y + 4 * size, size)
+        sierpinski_carpet(x + 4 * size , y - 2 * size, size)
+        sierpinski_carpet(x - 2 * size , y - 2 * size, size)
+        sierpinski_carpet(x - 2 * size , y + 4 * size, size) 
+
+sierpinski_carpet(200, 200, size)
+
+
+
+
+
+
+root.mainloop()
