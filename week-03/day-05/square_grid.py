df0b033af99edc592b86cbef8e4771e4fa9f7bc8
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-22 13:17:50 +0200

    feat(square_grid.py): random color + timer

--- week-03/day-05/square_grid.py
+++ week-03/day-05/square_grid.py
@@ -1,11 +1,17 @@
 from tkinter import *
-
+import time
+import random
 root = Tk()
 root.configure(background ='black')
 canvas = Canvas(root, width='600', height='600')
 canvas.pack()
+
 def mother_square(x, y, size, width):
-    square = canvas.create_rectangle(x, y, x + size, y + size, outline = "black", fill="", width = width)
+    r = lambda: random.randint(0,255)
+    random_color = '#%02X%02X%02X' % (r(),r(),r())
+    time.sleep(0.1)
+    canvas.update()
+    square = canvas.create_rectangle(x, y, x + size, y + size, outline = random_color, fill="", width = width)
     if size < 10:
         return 
     else:
