0f3584210e884e20865c1b038e470c08d8a365cb
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-25 14:11:16 +0200

    feat(counter.py): complete

---
+++ week-04/day-01/counter.py
@@ -0,0 +1,26 @@
+# Create Counter class
+# which has an integer field value
+# when creating it should have a default value 0 or we can specify it when creating
+# we can add(number) to this counter another whole number
+# or we can add() without parameters just increasing the counter's value by one
+# and we can get() the current value
+# also we can reset() the value to the initial value
+# Check if everything is working fine with the proper test
+# Download test_counter.py and place it next to your solution
+# Run the test file as a usual python program
+class Counter():
+    def __init__(self, start = 0):
+        self.integer = start
+
+    def add(self, number = None):
+        if number is None:
+            self.integer +=1
+        else:
+            self.integer += int(number)
+    
+    def get(self):
+        return self.integer
+    
+    def reset(self):
+        self.__init__()
+  
