40fffea6397bd744eba720e7dcacba704f76b1a3
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-25 12:35:00 +0200

    feat(fleet_of_things.py): complete

---
+++ week-04/day-01/fleet_of_things.py
@@ -0,0 +1,22 @@
+from fleet import Fleet
+from thing import Thing
+
+fleet = Fleet()
+# Create a fleet of things to have this output:
+# 1. [ ] Get milk
+# 2. [ ] Remove the obstacles
+# 3. [x] Stand up
+# 4. [x] Eat lunch
+
+item_1 = Thing("Get milk")
+print(item_1)
+item_2 = Thing("Remove the obstacles")
+print(item_2)
+item_3 = Thing("Stand up")
+item_3.complete()
+print(item_3)
+item_4 =Thing("Eat Lunch")
+item_4.complete()
+print(item_4)
+
+print(fleet)
