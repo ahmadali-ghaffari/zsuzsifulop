eb149251ad9d4336a4d92318e7d43acb1d4943a7
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-27 12:40:58 +0200

    feat(get_apple_test.py): fixed

--- week-04/day-03/get_apple_test.py
+++ week-04/day-03/get_apple_test.py
@@ -1,9 +1,11 @@
 import unittest
 from get_apple import Apple
 
+test_apple = Apple()
+
 class AppleTest(unittest.TestCase):
-    def apple_test(self):
-        self.assertEqual(1, 1)
+    def test_apple(self):
+        self.assertEqual(test_apple.get_apple(), "apple")
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(exit=False)
