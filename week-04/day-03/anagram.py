d5044b8b9028f4ae3e04774f0d37f7414bcce534
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-27 13:42:55 +0200

    feat(anagram.py): complete

---
+++ week-04/day-03/anagram.py
@@ -0,0 +1,9 @@
+word1_global = "hajok"
+word2_global = "koahj"
+class Anagram:
+    def anagram_from_words(self, word1, word2):
+        if sorted(word1) == sorted(word2):
+            return True
+        else:
+            return False
+
