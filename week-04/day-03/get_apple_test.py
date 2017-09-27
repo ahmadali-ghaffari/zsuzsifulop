1f0d2c705ea3143c2eca65a01427a93ab497942a
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-27 11:53:41 +0200

    feat(get_apple.py): complete

---
+++ week-04/day-03/get_apple_test.py
@@ -0,0 +1,9 @@
+import unittest
+from get_apple import Apple
+
+class AppleTest(unittest.TestCase):
+    def apple_test(self):
+        self.assertEqual(1, 1)
+
+if __name__ == "__main__":
+    unittest.main()
