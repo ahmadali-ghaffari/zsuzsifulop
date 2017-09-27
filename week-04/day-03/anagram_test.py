ea906839c7e95e99b45dd3db41fcd1b8393cb3f6
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-27 13:47:38 +0200

    feat(anagram_test.py): complete

---
+++ week-04/day-03/anagram_test.py
@@ -0,0 +1,11 @@
+import unittest
+from anagram import Anagram
+
+anagram = Anagram()
+
+class AnagramTest(unittest.TestCase):
+    def test_anagram(self):
+        self.assertTrue(anagram.anagram_from_words("es", "se"))
+
+if __name__ == "__main__":
+    unittest.main()
