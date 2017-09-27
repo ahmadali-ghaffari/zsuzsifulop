0d3d35a99deb015a4c5c318eb7e880e3ccafc0b8
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-27 16:46:39 +0200

    feat(animal_test.py): complete

---
+++ week-04/day-03/animal_test.py
@@ -0,0 +1,21 @@
+import unittest
+from animal import Animal
+
+animal = Animal()
+class Animal_test(unittest.TestCase):
+
+    def test_eat(self):
+        animal.eat()
+        self.assertEqual(animal.hunger, 49)
+    
+    def test_drink(self):
+        animal.drink()
+        self.assertEqual(animal.thirst, 49)
+
+    def test_play(self):
+        animal.play()
+        self.assertEqual(animal.hunger, 50)
+        self.assertEqual(animal.thirst, 50)
+
+if __name__ == "__main__":
+    unittest.main()
