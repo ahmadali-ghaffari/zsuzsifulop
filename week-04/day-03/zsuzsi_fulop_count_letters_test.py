cbaf47c32da9fbbcf332483865a6a04d7425cda4
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-27 15:00:02 +0200

    feat(zsuzsi_fulop_count_letters_test): complete

---
+++ week-04/day-03/zsuzsi_fulop_count_letters_test.py
@@ -0,0 +1,11 @@
+import unittest
+from zsuzsi_fulop_count_letters import count_letter
+
+count_letter2 = count_letter()
+
+class Test_count_letters(unittest.TestCase):
+    def test_count(self):
+        self.assertEqual(count_letter2.count_the_letters("baa"),{"a":2, "b":1})
+
+if __name__ == "__main__":
+    unittest.main()
