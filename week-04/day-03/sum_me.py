84fde0c1af27d44ed99ef0e5e00bfad882da0879
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-27 12:39:57 +0200

    feat(sum_me.py): complete

---
+++ week-04/day-03/sum_me.py
@@ -0,0 +1,7 @@
+class Sumclass:
+    sum_list = [1, 4, 3]
+    def sum_function(self, sum_list):
+        sum = 0
+        for elements in sum_list:
+            sum += elements 
+        return sum
