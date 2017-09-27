078f6c055ece99badd84bf7d69852457820bc4a0
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-27 16:35:02 +0200

    feat(sharpie_test.py): complete

---
+++ week-04/day-03/sharpie_test.py
@@ -0,0 +1,13 @@
+import unittest
+from sharpie import Sharpie
+
+sharpie = Sharpie("red", 27)
+
+class Sharpie_Test(unittest.TestCase):
+    
+    def test_use(self):
+        sharpie.use()
+        
+        self.assertEqual(sharpie.ink_amount,2)
+if __name__ == "__main__":
+    unittest.main()
