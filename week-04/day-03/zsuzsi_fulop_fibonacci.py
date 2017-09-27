43991641d10d9afa71e0be9fe74133af1b3dc105
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-27 15:40:03 +0200

    feat(zsuzsi_fulop_fibonacci.py): complete

---
+++ week-04/day-03/zsuzsi_fulop_fibonacci.py
@@ -0,0 +1,9 @@
+class Fibonacci:
+    def fibonacci(self, number):
+        if number == 0:
+            return 0
+        elif number == 1:
+            return 1
+        else: 
+            return (self.fibonacci(number - 1) + self.fibonacci(number - 2))
+            
