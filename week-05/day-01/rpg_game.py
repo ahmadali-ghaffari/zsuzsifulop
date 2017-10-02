49e5b6e7effda235d8cae18cf7cd3c32a6d71ef3
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-10-02 16:00:39 +0200

    feat(rpg_game): moving hero

--- week-05/day-01/rpg_game.py
+++ week-05/day-01/rpg_game.py
@@ -7,6 +7,11 @@ root = Tk()
 
 image1 = PhotoImage(file = 'floor.png')
 image2 = PhotoImage(file = 'wall.Png')
+hero_down = PhotoImage(file = 'hero-down.png')
+hero_up = PhotoImage(file = 'hero-up.png')
+hero_left = PhotoImage(file = 'hero-left.png')
+hero_right = PhotoImage(file = 'hero-right.png')
+
 canvas = Canvas(root, width = 700, height = 770, bg = 'white')
 canvas.pack()
 image_floor = canvas.create_image(35, 35, image = image1)
@@ -27,6 +32,38 @@ def map_draw():
             elif lines[elements] == '0':
                 image_floor = canvas.create_image(elements * 70 + 35, a + 35, image = image1)
         a += 70
-    #image_floor = canvas.create_image(1 * 70 + 35, 0 + 35, image = image1)
 map_draw()
+
+class Entity(object):
+    def __init__(self):
+        self.x = 35
+        self.y = 35
+
+    def entity_draw(self, entity_image):
+        self.image_entity = canvas.create_image(self.x, self.y, image = entity_image)
+    
+    def move(self, dx, dy):
+        canvas.move(self.image_entity, dx, dy )
+
+def on_key_press(e):
+    if ( e.keysym == 'Up' ):
+        hero.move(0,-70)
+    elif( e.keysym == 'Down' ):
+        hero.move(0,70)
+    elif( e.keysym == 'Right' ):
+        hero.move(70,0)
+    elif( e.keysym == 'Left' ):
+        hero.move(-70,0)
+   
+
+        
+class Hero(Entity):
+    def __init__(self):
+        pass
+
+hero = Entity()
+hero.entity_draw(hero_down)
+
+root.bind("<KeyPress>", on_key_press)
 root.mainloop()
+
