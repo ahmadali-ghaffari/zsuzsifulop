318795cdb0c6d20bfabd2e0742dacaed5570f694
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-10-17 22:41:41 +0200

    feat(poker-test.py): update

--- week-06/day-02/poker-test.py
+++ week-06/day-02/poker-test.py
@@ -1,15 +1,20 @@
 import unittest
 from poker import Card
-from poker import Hands
-hands = Hands()
+from poker import Deck 
+
+deck = Deck()
 
 class TestPoker(unittest.TestCase):
-    def test_check_cardtype(self):
-        assert type(hands.create_random_card() is Card)
+    # def test_check_cardtype(self):
+    #     assert type(deck.create_random_card() is Card)
+    def test_make_deck(self):
+        self.assertEqual(deck.make_deck(), 52)
 
-    def test_fill_up_hand(self):
-        self.assertEqual(hands.fill_up_hands(), 5)
-        
+    def test_shuffle(self):
+        self.assertNotEqual(deck.shuffle(), "Card:D2")
+    
+    def test_choice_card(self):
+        self.assertEqual(deck.choice_card(), 51)
     
 if __name__ == '__main__':
     unittest.main()
