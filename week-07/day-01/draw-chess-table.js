4e84c77d22dcfdf820dbc071d355ae03698a2b33
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-10-24 16:58:56 +0200

    feat(task 28, 31, 32, 35): complete

---
+++ week-07/day-01/draw-chess-table.js
@@ -0,0 +1,25 @@
+
+// Crate a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % % 
+//  % % % %
+//
+
+var style01 ="% % % %";
+var style02 =" % % % %";
+
+var i;
+
+for (i=1; i<9; i++){
+    if (i%2 === 0){
+        console.log(style01);
+    } else{
+        console.log(style02);
+    }
+}
---
+++ week-07/day-01/draw-diagonals.js
@@ -0,0 +1,17 @@
+var lineCount = 6;
+var i;
+var j;
+var string ="";
+
+for (i = 0; i < lineCount; i++){
+    for (j = 0; j < lineCount; j++) {
+        if (i === 0 || j === 0 || j === lineCount-1 || i ===lineCount-1 || i === j){
+            string +="%";
+        } else {
+            string += " ";
+        }
+    
+    }
+    console.log(string);
+    string = "";
+}
