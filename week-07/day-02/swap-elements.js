b58656a26b28e33bc6b711f8193dc4e00c4404a8
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-10-25 11:25:11 +0200

    feat(swap-elements.js): complete

---
+++ week-07/day-02/swap-elements.js
@@ -0,0 +1,14 @@
+'use strict';
+// - Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// - Swap the first and the third element of `abc`
+
+var abc = ["Arthur", "Boe", "Chloe"];
+
+var swapArrayElements = function(arr, indexA, indexB) {
+    var temp = arr[indexA];
+    arr[indexA] = arr[indexB];
+    arr[indexB] = temp;
+};
+
+swapArrayElements(abc, 0, 2);
+console.log(abc)
