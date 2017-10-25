a12c8cadb35d51a635e74c2762baeaabe401c489
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-10-25 12:28:24 +0200

    feat(printer.js): complete

---
+++ week-07/day-02/printer.js
@@ -0,0 +1,14 @@
+'use strict';
+// - Create a function called `printer`
+//   which logs to the console the input parameters
+//   (can have multiple number of arguments)
+
+var parameters_to_write_out = ["Zsuzsi", "Fülöp"];
+function printer(parameter){
+    var i = 0;
+    for (i = 0; i < parameter.length; i++){
+        console.log(parameter[i]);
+    }
+}
+
+printer(parameters_to_write_out)
