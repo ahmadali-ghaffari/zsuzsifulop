aeb45f32e353d0ea139fd28c237f2b03ad95a414
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-10-25 12:24:27 +0200

    feat(factorio.js): complete

---
+++ week-07/day-02/factorio.js
@@ -0,0 +1,15 @@
+'use strict';
+// - Create a function called `factorio`
+//   that returns it's input's factorial
+
+var numbers = [1, 2, 3, 7];
+var factorial_value = 1;
+function factorial(numbers_to_multiply) {
+    var i = 0;
+    for (i = 0; i < numbers_to_multiply.length; i++){
+        factorial_value *= numbers_to_multiply[i];
+    }
+    console.log(factorial_value);
+    return factorial_value;
+}
+factorial(numbers);
