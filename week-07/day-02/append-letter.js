ee8b5d05b7972526c358e82a439f7d1caa0da061
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-10-25 14:56:09 +0200

    feat(append-letter.js): complete

---
+++ week-07/day-02/append-letter.js
@@ -0,0 +1,9 @@
+'use strict';
+// Add "a" to every string in far
+
+var far = ["kuty", "macsk", "kacs", "rók", "halacsk"];
+    far = far.map(function(x) {
+    return x + 'a';
+});
+
+console.log(far);
