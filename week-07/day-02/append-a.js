b8a95897f99d51405498efa0fca57f1a9340aad4
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-10-25 13:33:27 +0200

    feat(append-a.js): complete

---
+++ week-07/day-02/append-a.js
@@ -0,0 +1,13 @@
+'use strict';
+// - Create a variable named `nimals`
+//   with the following content: `["kuty", "macsk", "cic"]`
+// - Add all elements an `"a"` at the end
+// - try to use built in functions instead of loops
+
+var nimals = ["kuty", "macsk", "cic"];
+
+var plus_a = nimals.map(function(x) {
+    return x + 'a';
+});
+
+console.log(plus_a);
