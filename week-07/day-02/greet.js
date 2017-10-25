c7a4999f2b10bc74be08136513dee6a6b404adce
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-10-25 11:59:59 +0200

    feat(greet.js): complete

---
+++ week-07/day-02/greet.js
@@ -0,0 +1,17 @@
+'use strict';
+// - Create variable named `al` and assign the value `Greenfox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings, dear Greenfox`
+//     - Prepare for the special case when no parameters are given
+// - Greet `al`
+
+var al = 'Greenfox';
+function greet(text){
+    if (al.length > 0){
+    console.log('Greetings, dear ' + text);
+    } else {
+        console.log("Greating is not possible!")
+    }
+}
+
+greet(al);
