ec7c74143f50a4f6d1b65aaee706ef552a8ff843
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-10-25 15:29:57 +0200

    feat(element-finder.js)

---
+++ week-07/day-02/element-finder.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Check if the array contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+
+var numbers = [1,2,3,4,5,7,8];
+var i = 0;
+var x = false;
+
+for (i = 0; i < numbers.length; i++){
+    if (numbers[i] === 7){
+        x = true
+    } 
+}
+if (x) {
+    console.log("Huray");
+} else{
+    console.log("Nooo");
+}
