f0456294089062deb4ac2ea202bb4c11d472e24d
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-10-31 16:49:12 +0100

    feat(csv-tp-json.js): complete

---
+++ week-08/day-02/csv-to-json.js
@@ -0,0 +1,46 @@
+const data = `Phasianus colchicus;9442;Fuscia
+Pedetes capensis;2869;Puce
+Recurvirostra avosetta;4285;Fuscia
+Kobus defassa;4726;Red
+Anser anser;8163;Violet
+Callipepla gambelii;4422;Fuscia
+Ara ararauna;7935;Aquamarine
+Ara ararauna;3081;Crimson
+Pelecanus conspicillatus;9831;Mauv
+Cynictis penicillata;839;Puce
+Graspus graspus;9814;Indigo
+Vicugna vicugna;6439;Violet
+Lemur catta;2023;Pink
+Tragelaphus scriptus;986;Aquamarine
+Corythornis cristata;4995;Red
+Sitta canadensis;6786;Puce
+Sus scrofa;9338;Goldenrod
+Psophia viridis;8881;Yellow
+Gabianus pacificus;7714;Aquamarine
+Sus scrofa;6449;Orange
+Pteropus rufus;5816;Yellow
+Macropus agilis;1241;Teal
+Pavo cristatus;5860;Violet
+Corythornis cristata;5358;Khaki
+Loris tardigratus;725;Orange`;
+
+// Convert the above string (which is in CSV format) to an array of objects
+// Columns should map to the following object:
+//  { name: "Macropus agilis", id: 1241, color_code: "Teal" }
+var lines = data.split("\n")
+
+namelist = []
+var myMap = lines.map(function(item){
+    var oneRegistery = item.split(";")
+    if (!namelist.includes(oneRegistery[0])){
+        namelist.push(oneRegistery[0])
+        return {name: oneRegistery[0], id: oneRegistery[1], color_code: oneRegistery[2]};
+    }
+});
+var myMapFilter = myMap.filter(function(item){
+     return item != undefined;
+});
+console.log(myMapFilter);
+
+// Remove all duplicates based on their name, always keep the first one
+// Don't use for or forEach
