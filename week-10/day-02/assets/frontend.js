26b431aeff8340d22c2949ec09b0e6e0046c7021
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-11-16 13:20:14 +0100

    feat(frontend.js): working playlist with tracklist

--- week-10/day-02/assets/frontend.js
+++ week-10/day-02/assets/frontend.js
@@ -1,25 +1,34 @@
 const Playlist = function(){
     var root = document.querySelector(".playlists")
+    var rootsongs = document.querySelector(".songlists")
     var plus = document.querySelector(".plus");
+    var firstline = document.querySelector(".firstline");
     plus.addEventListener("click", function(){
         window.prompt("New tracklist!")
     })
-    function showCreateDialog(){
-    };
     
     function create(nameMe){
         ajax('POST', 'http://localhost:8080/playlist', console.log, {"playlist" :  nameMe });
     };
 
+    function load(){
+        ajax('GET', 'http://localhost:8080/favourite', render)
+    };
+
+    function load2(i){
+        ajax('GET', 'http://localhost:8080/playlist/' + i, render2)
+    }; 
+
     let render = function (response){
         while (root.firstChild){
             root.removeChild(root.firstChild);
         }
+        console.log(response)
         response.forEach(function(element, i) {
             console.log(response)
             let li = document.createElement('li');
             li.innerHTML = element.playlist;  
-            li.setAttribute('class', 'song');      
+            li.setAttribute('class', 'track song');      
             root.appendChild(li);
             let X = document.createElement("span");
             X.setAttribute('class', 'close');
@@ -29,7 +38,11 @@ const Playlist = function(){
             });
             li.appendChild(X);
             li.addEventListener('click', function(){
-                highlight(i)});
+                firstline.textContent = element.playlist;
+                highlight(i);
+                rootsongs.innerHTML="";
+                load2(i+1);
+                });
             }); 
     };
 
@@ -38,37 +51,45 @@ const Playlist = function(){
     };
     
     function highlight(index){
-        let myList = root.querySelectorAll("li")
+        let myList = root.querySelectorAll(".track")
         console.log(myList)
         myList.forEach(function(element, i) {
-            element.setAttribute('class', "song")
+            element.setAttribute('class', "track song")
             if (index === i){
-                element.setAttribute('class', "song blue")
+                element.setAttribute('class', "track song blue")
             };
         });
     };
 
-    function addEvents(){
-    }; 
-
-    function load(){
-        ajax('GET', 'http://localhost:8080/favourite', render)
-    };
-
-    function clickHandles(){
+    function render2(response){
+        response.forEach(function(element) {
+            let li = document.createElement('li');
+            li.innerHTML = element.path;  
+            li.setAttribute('class', 'song');      
+            rootsongs.appendChild(li);
+            li.addEventListener('click', function(){
+                var audio = document.querySelector(".playline")
+                audio.setAttribute('src', element.path);
+                var secondline = document.querySelector(".secondline");
+                secondline.textContent = element.path;
+            });
+        });
     }; 
 
     return {
         render: render,
         highlight: highlight,
-        addEvents: addEvents,
         delete1: delete1,
         create: create,
-        load: load
+        load: load,
+        render2: render2,
+        load2: load2
     }
 };
-
+    
 let playlistmodule =  Playlist();
-playlistmodule.load() 
-playlistmodule.highlight()
-playlistmodule.create("Zsuzsi")
+playlistmodule.highlight();
+playlistmodule.load();
+playlistmodule.load2();
+playlistmodule.highlight();
+
