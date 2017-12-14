fff262fed3d5ee6bac0b54573e2284e731da6d20
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 13:10:22 +0100

    style(seconds_in_a_day.py): cleaner code

--- week-02/day-01/seconds_in_a_day.py
+++ week-02/day-01/seconds_in_a_day.py
@@ -2,9 +2,9 @@
 # day if the current time is represented bt the variables
 #print(current_time_in_seconds)
 
-current_hours = 14;
-current_minutes = 34;
-current_seconds = 42;
+current_hours = 14
+current_minutes = 34
+current_seconds = 42
 
 current_hour_in_seconds = current_hours * 60 * 60
 current_minutes_in_seconds = current_minutes * 60
