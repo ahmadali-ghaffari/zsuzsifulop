4c1fd7a0f0908800eb73647a0e6b3eb6b7c3adef
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 13:05:31 +0100

    style(one_two_a_lot.py): cleaner code

--- week-02/day-01/one_two_a_lot.py
+++ week-02/day-01/one_two_a_lot.py
@@ -1,14 +1,16 @@
 # Write a program that reads a number form the standard input,
-c=int(input("Hey, give me a number!"))
 # If the number is zero or smaller it should print: Not enough
-if c<=0:
-    print("Not  enought")
-elif c==1:
-    print("One")
-elif c==2:
-    print("Two")
-else:
-    print("A lot")
 # If the number is one it should print: One
 # If the number is two it should print: Two
 # If the number is more than two it should print: A lot
+
+c = int(input("Hey, give me a number!"))
+if c <= 0:
+    print("Not enought")
+elif c == 1:
+    print("One")
+elif c == 2:
+    print("Two")
+else:
+    print("A lot")
+
