c2d120e306fb2b0bcb389d619145a030e592ac64
Zsuzsa Fülöp <flp.zsuzsi@gmail.com>
2017-12-14 15:34:26 +0100

    Delete swap_elemets.py

--- week-02/day-02/swap_elemets.py
+++
@@ -1,6 +0,0 @@
-# - Create a variable named `abc`
-#   with the following content: `["first", "second", "third"]`
-# - Swap the first and the third element of `abc`
-abc = ["first", "second", "third"]
-abc.reverse()
-print(abc)
