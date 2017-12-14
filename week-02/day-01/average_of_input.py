e07bbc857860fe34dad2968677f2f1f78c9f527d
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 12:32:03 +0100

    style(average_of_input.py): cleaner code

--- week-02/day-01/average_of_input.py
+++ week-02/day-01/average_of_input.py
@@ -2,8 +2,9 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
-sum=0
+
+sum = 0
 for i in range(0,5):
     sum += int(input("Give me the number!"))
-print("Sum: " + str(sum) + " Average: " + str(sum/(i-1)) )
+print("Sum: " + str(sum) + " Average: " + str(sum/(i-1)))
    
