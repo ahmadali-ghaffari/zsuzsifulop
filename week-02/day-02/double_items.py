63d594e9ed398d5d8fa45093b96c0e4ae4e13237
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 15:25:50 +0100

    style(double_items): cleaner code

--- week-02/day-02/double_items.py
+++ week-02/day-02/double_items.py
@@ -2,11 +2,8 @@
 #   with the following content: `[3, 4, 5, 6, 7]`
 # - Double all the values in the array
 
-
-
 ag = [3, 4, 5, 6, 7]
 
 for i in range(len(ag)):
-    ag[i] = ag[i]*2
-    
+    ag[i] = ag[i] * 2
 print(ag)
