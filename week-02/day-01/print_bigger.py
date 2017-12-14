4a9a76d99b53c305d6e54205f8ee84609759ddaa
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 13:07:58 +0100

    style(print_bigger.py): cleaner code

--- week-02/day-01/print_bigger.py
+++ week-02/day-01/print_bigger.py
@@ -1,7 +1,8 @@
 # Write a program that asks for two numbers and prints the bigger one
-a=int(input("Give me the first number!"))
-b=int(input("Give me the second number!"))
-if a>b:
+
+a = int(input("Give me the first number!"))
+b = int(input("Give me the second number!"))
+if a > b:
     print(str(a) + " is bigger than " + str(b))
 else:
-    print(str(b)+" is bigger than " + str(a))
+    print(str(b) +" is bigger than " + str(a))
