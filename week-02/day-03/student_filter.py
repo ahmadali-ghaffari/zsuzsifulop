a609082e8651661aef2e844dab39a948abe0bdf4
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 15:17:30 +0100

    style(student_filter.py): cleaner code

--- week-02/day-03/student_filter.py
+++ week-02/day-03/student_filter.py
@@ -1,3 +1,7 @@
+# create a function that takes a list of students and prints:
+# - Who has got more candies than 4 candies
+# create a function that takes a list of students and prints: 
+#  - how many candies they have on average
 
 students = [
         {'name': 'Rezso', 'age': 9.5, 'candies': 2},
@@ -6,20 +10,18 @@ students = [
         {'name': 'Zsombor', 'age': 12, 'candies': 5}
 ]
 
-# create a function that takes a list of students and prints:
-# - Who has got more candies than 4 candies
 def who_more():
     for i in range(len(students)):
         if students[i]['candies'] > 4:
             print(students[i]['name'])
 
-who_more()
-# create a function that takes a list of students and prints: 
-#  - how many candies they have on average
+
 def candi_avi():
-    sum=0
+    sum = 0
     for i in range(len(students)):
-        sum +=students[i]['candies']/len(students)
+        sum += students[i]['candies'] / len(students)
     print(sum)
 
+
+who_more()
 candi_avi()
