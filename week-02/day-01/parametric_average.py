4a1d1efe99c85dc245712d9e99b71b5f0735d7c0
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 13:06:20 +0100

    style(parametric_average.py): cleaner code

--- week-02/day-01/parametric_average.py
+++ week-02/day-01/parametric_average.py
@@ -5,10 +5,10 @@
 #
 # Sum: 22, Average: 4.4
 
-sum=0
-d= int(input("Give me a repeat number!"))
+sum = 0
+d = int(input("Give me a repeat number!"))
 for i in range(0,d):
     sum += int(input("Give me the number!"))
-print("Sum: " + str(sum) + " Average: " + str(sum/(i-1)) )
+print("Sum: " + str(sum) + " Average: " + str(sum / (i-1)) )
    
 
