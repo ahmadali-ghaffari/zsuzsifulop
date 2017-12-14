953bbfdd2706dc20a7a5f1c6342a433a1dc10d55
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 14:48:08 +0100

    style(bank_transfer.py): cleaner code

--- week-02/day-03/bank_transfer.py
+++ week-02/day-03/bank_transfer.py
@@ -1,13 +1,16 @@
+# Create function that returns the name and balance of cash on an account
+
 accounts = [
 	{ 'client_name': 'Igor', 'account_number': 111, 'balance': 10.5 },
 	{ 'client_name': 'Vladimir', 'account_number': 222, 'balance': 20.5},
 	{ 'client_name': 'Sergei', 'account_number': 333, 'balance': 30 }
 ]
 
-# Create function that returns the name and balance of cash on an account
+
 account_from = int(input("Give me the bank account number to transfer!"))
 account_to = int(input("Give me the bank account number to recive!"))
 amount_to_transfer = float(input("Give me the amount!"))
+
 def banking ():
     something_goes_wrong = False
     if accounts[0]['account_number'] != account_from and accounts[1]['account_number'] != account_from and accounts[2]['account_number'] != account_from:
@@ -18,21 +21,16 @@ def banking ():
         something_goes_wrong = True
     elif something_goes_wrong == False:
         for i in range(len(accounts)):
-            
             if accounts[i]['account_number'] == account_from:
                 if accounts[i]['balance'] >= amount_to_transfer:
                     new_balance = accounts[i]['balance'] - amount_to_transfer
                     print(new_balance)
-            
         for j in range(len(accounts)):
             flag2 = False
             if accounts[j]['account_number'] == account_to:
                 new_balance_2 = accounts[j]['balance'] + amount_to_transfer
                 print(new_balance_2)
-     
-       
-        
-            
+
 banking()
 
 
