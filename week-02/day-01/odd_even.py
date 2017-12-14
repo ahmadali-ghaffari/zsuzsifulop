068734fc88675b250bc8c1c30a6febb0dccf0134
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 13:04:34 +0100

    feat(odd_even.py): cleaner code

--- week-02/day-01/odd_even.py
+++ week-02/day-01/odd_even.py
@@ -1,7 +1,8 @@
 # Write a program that reads a number form the standard input,
 # Than prints "Odd" if the number is odd, or "Even" it it is even.
-c=int(input("Please, give me a number"))
-if c%2 == 0:
+
+c = int(input("Please, give me a number"))
+if c % 2 == 0:
     print("Hey, I am even")
 else:
     print("Hey, I am odd")
