303bac604e89df8aa8d36742e53792f51954cf64
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 12:53:02 +0100

    style(conditional_variable_mutation.py): cleaner code

--- week-02/day-01/conditional_variable_mutation.py
+++ week-02/day-01/conditional_variable_mutation.py
@@ -1,65 +1,57 @@
 a = 24
+b = 13
+c = 123
+d = 8
 out = 0
+out2 = ""
+out3 = ""
+credits = 100
+is_bonus = False
+time = 120
+
 # if a is even increment out by one
-if 24%2 ==0:
+
+if 24 % 2 == 0:
     out += 1
-
-
 print(out)
 
-
-
-
-b = 13
-out2 = ""
 # if b is between 10 and 20 set out2 to "Sweet!"
 # if less than 10 set out2 to "More!",
 # if more than 20 set out2 to "Less!"
-if b>10 and b<20:
-    print("Sweet!")
-if b<10:
-    print("More")
-if b>20:
-    print("Less")
 
+if b > 10 and b < 20:
+    print("Sweet!")
+if b < 10:
+    print("More")
+if b > 20:
+    print("Less")
 print(out2)
 
-
-
-c = 123
-credits = 100
-is_bonus = False
 # if credits are at least 50,
 # and is_bonus is false decrement c by 2
 # if credits are smaller than 50,
 # and is_bonus is false decrement c by 1
 # if is_bonus is true c should remain the same
-if credits > 49 and is_bonus == False:
-    c -=2
-if credits < 50 and is_bonus == False:
-    c -=1
-if is_bonus == True:
-    c ==c
 
+if credits > 49 and is_bonus == False:
+    c -= 2
+if credits < 50 and is_bonus == False:
+    c -= 1
+if is_bonus == True:
+    c == c
 print(c)
 
-
-
-
-d = 8
-time = 120
-out3 = ""
 # if d is dividable by 4
 # and time is not more than 200
 # set out3 to "check"
 # if time is more than 200
 # set out3 to "Time out"
 # otherwise set out3 to "Run Forest Run!"
-if d % 4 == 0 and time > 200:
-    out3="check"
-if time>200:
-    out="Time out"
-else:
-    out="Run forest Run"
 
+if d % 4 == 0 and time > 200:
+    out3 = "check"
+if time > 200:
+    out = "Time out"
+else:
+    out = "Run forest Run"
 print(out3)
