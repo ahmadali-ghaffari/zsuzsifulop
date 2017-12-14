b0f813201d4a34b559699e3c90d6f7ff6453549f
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 12:57:51 +0100

    style(draw_square.py): cleaner code

--- week-02/day-01/draw_square.py
+++ week-02/day-01/draw_square.py
@@ -10,11 +10,11 @@
 # %%%%%
 #
 # The square should have as many lines as the number was
-c=6
-h="%"
-s=" "
-for i in range (1,c+1):
-    if i==1 or i==c:
-        print(h*c)
+c = 6
+h = "%"
+s = " "
+for i in range (1,c + 1):
+    if i == 1 or i == c:
+        print(h * c)
     elif i != 1 or i != c:
-        print(h +(c-2)*s+h)
+        print(h + (c - 2) * s + h)
