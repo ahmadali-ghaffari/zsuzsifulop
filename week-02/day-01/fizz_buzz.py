cb7f527b1f04c4a24731292dff8164e0773a0eaa
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 12:59:09 +0100

    style(fizz_buzz.py): cleaner code

--- week-02/day-01/fizz_buzz.py
+++ week-02/day-01/fizz_buzz.py
@@ -2,11 +2,12 @@
 # But for multiples of three print �Fizz� instead of the number
 # and for the multiples of five print �Buzz�.
 # For numbers which are multiples of both three and five print �FizzBuzz�.
+
 for x in range(1,101):
-    if x%3 == 0:
+    if x % 3 == 0:
         print("Fizz")
-    if x%5 == 0:
+    if x % 5 == 0:
         print("Buzz")
-    if x%5 == 0 and x%3 == 0:
+    if x % 5 == 0 and x % 3 == 0:
         print("FizzBuzz") 
     print(x)
