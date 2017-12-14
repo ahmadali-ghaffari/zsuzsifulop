a8f5c5de09f9a37dad50911224de14bd947c81ff
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 15:30:08 +0100

    style(sum.py): cleaner code

--- week-02/day-02/sum.py
+++ week-02/day-02/sum.py
@@ -1,9 +1,11 @@
 # - Write a function called `sum` that sum all the numbers
 #   until the given parameter
+
 def sum(*numbers):
-    total=0
+    total = 0
     for i in numbers:
         total += i
     print (total)
+
 
 sum(5, 4, 7)
