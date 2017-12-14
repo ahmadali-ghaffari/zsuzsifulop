651619bc8fcef9ef85b671ef29dc16cfeb9735d0
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 12:32:47 +0100

    style(bmi.py): cleaner code

--- week-02/day-01/bmi.py
+++ week-02/day-01/bmi.py
@@ -1,7 +1,6 @@
-
-massInKg = 67.0
-heightInM = 1.7
-
 #Print the Body mass index (BMI) based on these values
-BMI=massInKg/(heightInM **2)
+
+massInKg = 64.0
+heightInM = 1.7
+BMI = massInKg / (heightInM ** 2)
 print(BMI)
