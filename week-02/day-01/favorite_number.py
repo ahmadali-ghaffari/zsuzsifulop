f51889c3c7fbfe9fecfa8dca83099f881da596bc
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 12:48:41 +0100

    style(favorite_number.py): cleaner code

--- week-02/day-01/favorite_number.py
+++ week-02/day-01/favorite_number.py
@@ -1,5 +1,6 @@
-favorite_number = 8
 # Store your favorite number in a variable (as a number)
 # And print it like this: "My favorite number is: 8"
+
+favorite_number = 8
 a = 27
 print ("My favourite number is: " + str(a))
