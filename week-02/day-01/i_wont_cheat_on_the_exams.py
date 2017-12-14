714f00dae82c05134da3e6750c08e6441bb64287
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 13:01:43 +0100

    style(i_wont_cheat_on_the_exams): cleaner code

--- week-02/day-01/i_wont_cheat_on_the_exams.py
+++ week-02/day-01/i_wont_cheat_on_the_exams.py
@@ -1,5 +1,6 @@
 # Create a program that writes this line 100 times:
 # "I won't cheat on the exam!"
-k="I won't cheat on the exam!"
+
+k = "I won't cheat on the exam!"
 for i in range(0,101):
     print(k)
