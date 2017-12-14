e3dca62b8b53433894a4f2135ba46d5d9ab7d75e
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 13:16:05 +0100

    style(variable_mutation.py): cleaner code

--- week-02/day-01/variable_mutation.py
+++ week-02/day-01/variable_mutation.py
@@ -1,97 +1,65 @@
 a = 3
 # make it bigger by 10
 a += 13
-
 print(a)
-
-
-
 
 b = 100
 # make it smaller by 7
-b -=7
-
+b -= 7
 print(b)
-
-
-
 
 c = 44
 # please double c's value
 c = 2 * c
-
 print(c)
-
-
-
 
 d = 125
 # please divide by 5 d's value 
-d = d/5
-
+d = d / 5
 print(d)
-
-
-
 
 e = 8
 # please cube of e's value
-e= 8 ** 3
-
+e = 8 ** 3
 print(e)
-
-
-
 
 f1 = 123
 f2 = 345
 # tell if f1 is bigger than f2 (pras a boolean)
-if f1>f2: 
+if f1 > f2: 
     print(True)
 else:
     print(False)
-
-
-
 
 g1 = 350
 g2 = 200
 # tell if the double of g2 is bigger than g1 (pras a boolean)
-if 2*g2>g1:
-    print (True)
+if 2 * g2 > g1:
+    print(True)
 else:
     print(False)
 
-
-
 h = 1357988018575474
 # tell if it has 11 as a divisor (pras a boolean)
-if h%11 ==0:
+if h % 11 == 0:
     print(True)
 else: 
     print(False)
-
-
-
 
 i1 = 10
 i2 = 3
 # tell if i1 is higher than i2 squared and smaller than i2 cubed (pras a boolean)
 if i1 ** 2 > i2 and i1 ** 2 < i2 ** 3:
-    print (True)
+    print(True)
 else: 
-    print (False)
-
-
+    print(False)
 
 j = 1521
 # tell if j is dividable by 3 or 5 (pras a boolean)
-if j%5 == 0 or j%3 == 0:
+if j % 5 == 0 or j % 3 == 0:
     print(True);
 else:
-    print (False)
-
-
+    print(False)
 
 k = "Apple"
 #fill the k variable with its cotnent 4 times
