7a18cb63357c59f962039f4f05891ff7b1b57530
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 13:13:16 +0100

    style(two_numbers.py): cleaner code

--- week-02/day-01/two_numbers.py
+++ week-02/day-01/two_numbers.py
@@ -1,21 +1,23 @@
 # Create a program that prints a few operations on two numbers: 22 and 13
-a=13 
-b=22
 # Print the result of 13 added to 22
-add = a+b
-print(add)
 # Print the result of 13 substracted from 22
-substracted = b-a
-print(substracted)
 # Print the result of 22 mutiplied by 13
-multiply=a*b
-print(multiply)
 # Print the result of 22 divided by 13 (as a decimal fraction)
-divide=b / a
-print(divide)
 # Print the integer part of 22 divided by 13
-div2=b // a
-print(div2)
 # Print the reminder of 22 divided by 13
-div3=b % a
+
+a = 13 
+b = 22
+
+add = a + b
+substracted = b - a
+multiply = a * b
+divide = b / a
+div2 = b // a
+div3 = b % a
+print(add)
+print(substracted)
+print(multiply)
+print(divide)
+print(div2)
 print(div3)
