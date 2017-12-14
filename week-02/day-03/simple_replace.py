92ce69f5ae0ee85be91e637fe54480a4e17d6e0b
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 15:18:01 +0100

    style(simple_replace.py): minor change

--- week-02/day-03/simple_replace.py
+++ week-02/day-03/simple_replace.py
@@ -2,5 +2,7 @@
 # I would like to replace "dishwasher" with "galaxy" in this example
 # Please fix it for me!
 # Expected ouput: In a galaxy far far away
+
 example={"diswasher":"galaxy"}
+
 print(example["diswasher"])
