9228e79908c675fb58bbfa65720ab56a16b2d2ab
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 15:05:10 +0100

    fix(reverse.py): less confusing variable name

--- week-02/day-03/reverse.py
+++ week-02/day-03/reverse.py
@@ -1,7 +1,9 @@
 # Reverse the string using StringBuilder
 
 reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
-def reverse(reversed):
-    string= reversed[::-1]
+
+def reverse(text):
+    string = text[::-1]
     print(string)
+
 reverse(reversed)
