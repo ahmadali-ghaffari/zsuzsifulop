ae4a8fb843e755071df223bc34cef229fcebdf8b
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-14 12:55:41 +0100

    style(hello_other.py): cleaner code

--- week-02/day-01/hello_others.py
+++ week-02/day-01/hello_others.py
@@ -1,6 +1,7 @@
 # Greet 3 of your classmates with this program, in three separate lines
 # like:
 #
-print ("Hello, Esther!")
-print ("Hello, Mary!")
-print ("Hello, Joe!")
+
+print("Hello, Esther!")
+print("Hello, Mary!")
+print("Hello, Joe!")
