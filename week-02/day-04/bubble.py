cfd82a3694848aa40a993a5eafbd138b99a0b836
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-15 13:47:02 +0100

    style(bubble.py): cleaner code

--- week-02/day-04/bubble.py
+++ week-02/day-04/bubble.py
@@ -1,18 +1,17 @@
-#buborékrendezés novekvő/illetve csökkenő módban 
+# bubble sort in both direction
+
 list = [34, 12, 24, 9, 5]
-#output = [5, 9, 12, 24, 34]
 
 def sorted(z):           
     if z == "asc":
-        for i in range(len(list)-1):
-            for j in range(i+1, len(list)):
+        for i in range(len(list) - 1):
+            for j in range(i + 1, len(list)):
                 if list[i] > list[j]:
                     list[i], list[j] = list[j], list[i]
         print(list)
-
     if z == "dec":
-        for i in range(len(list)-1):
-            for j in range(i+1, len(list)):
+        for i in range(len(list) - 1):
+            for j in range(i + 1, len(list)):
                 if list[i] < list[j]:
                     list[i], list[j] = list[j], list[i]
         print(list)
