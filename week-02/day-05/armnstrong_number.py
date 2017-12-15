da96543008bb07ae82fa018ef65454f36a7c123a
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-15 13:52:21 +0100

    style(armnstrong_number.py): cleaner code

--- week-02/day-05/armnstrong_number.py
+++ week-02/day-05/armnstrong_number.py
@@ -1,14 +1,12 @@
 number_global= "407"
 
 def armstrong(number):
-
     armstrong_number = 0
     length_of_the_number=len(number)
-
     for i in range(len(number)):
         armstrong_number += int(number[i]) ** length_of_the_number
-    
     if armstrong_number == int(number):
         print("The " + number + " is an Armstrong number!")
     
+
 armstrong(number_global)
