723db6beb500c8eee8abd0e930ada612084dc1ec
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-15 13:48:11 +0100

    style(unique.py): cleaner code

--- week-02/day-04/unique.py
+++ week-02/day-04/unique.py
@@ -1,13 +1,15 @@
-# az inputból szedje ki az ismétlődő elemeket
+# delete those elements which occurs twice or more
 #output = [1, 11, 34, 52, 61]
+
 list_of_numbers = [0, 5, 4, 3, 2, 1]
+
 def only_once(list_of_numbers):
     new_list = []
     for i in range(len(list_of_numbers)):
         flag = True
         for j in range(len(new_list)):
             if list_of_numbers[i] == new_list[j]:
-                flag=False
+                flag = False
         if flag == True:
             new_list.append(list_of_numbers[i])
     print(new_list)
