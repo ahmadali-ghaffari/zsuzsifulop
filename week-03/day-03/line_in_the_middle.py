375b4dfa50f76e88df428d43efad536695cd2a60
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-18 14:15:09 +0100

    style(line_in_the_middle.py): cleaner code

--- week-03/day-03/line_in_the_middle.py
+++ week-03/day-03/line_in_the_middle.py
@@ -1,12 +1,11 @@
+# draw a red horizontal line to the canvas' middle.
+# draw a green vertical line to the canvas' middle.
+
 from tkinter import *
 
 root = Tk()
-
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
 redline = canvas.create_line(0, 150, 300, 150, fill ="red")
 greenline = canvas.create_line(150, 0, 150, 300, fill = "green")
-# draw a red horizontal line to the canvas' middle.
-# draw a green vertical line to the canvas' middle.
-
 root.mainloop()
