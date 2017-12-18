aed7ebfbb942903aebeab1f74e25b4012ac82cf7
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-18 14:17:01 +0100

    style(purple_steps_3D.py): cleaner code

--- week-03/day-03/purple_steps_3D.py
+++ week-03/day-03/purple_steps_3D.py
@@ -1,14 +1,15 @@
-from tkinter import *
-
-root = Tk()
-
-canvas = Canvas(root, width='300', height='300')
-canvas.pack()
 
 # reproduce this:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
 
 #purple_square(10,10,25,25)
+
+from tkinter import *
+
+root = Tk()
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
 def purple_cubics():
     x_coordinate = 0
     y_coordinate = 0
@@ -18,7 +19,6 @@ def purple_cubics():
         x_coordinate = x_coordinate + size
         y_coordinate = y_coordinate + size
         size += 10
-
 
 
 purple_cubics()
