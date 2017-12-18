add104bb75c2f00dca46228a6f26f163a461f3ce
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-18 14:09:45 +0100

    style(diagonals.py): cleaner code

--- week-03/day-03/diagonals.py
+++ week-03/day-03/diagonals.py
@@ -1,11 +1,10 @@
+# draw the canvas' diagonals in green.
+
 from tkinter import *
 
 root = Tk()
-
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
-
-# draw the canvas' diagonals in green.
 first_diagonal = canvas.create_line(0, 0, 300, 300, fill = 'green')
 second_diagonal = canvas.create_line(0, 300, 300, 0, fill = 'green')
 root.mainloop()
