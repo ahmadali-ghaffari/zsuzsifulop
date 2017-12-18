6f97fa0ffe4500863c6b8ba30d0a8032f66f4312
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-18 14:11:22 +0100

    style(four_rectangles.py): cleaner code

--- week-03/day-03/four_rectangles.py
+++ week-03/day-03/four_rectangles.py
@@ -1,15 +1,12 @@
+# draw four different size and color rectangles.
+
 from tkinter import *
 
 root = Tk()
-
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
-
-# draw four different size and color rectangles.
-
 square = canvas.create_rectangle(145, 145, 155, 155, fill = "green")
 square2 = canvas.create_rectangle(1, 1, 130, 130, fill = "blue")
 square3 = canvas.create_rectangle(155, 155, 300, 300, fill = "yellow")
 square4 = canvas.create_rectangle(125, 130, 135, 15, fill = "pink")
-
 root.mainloop()
