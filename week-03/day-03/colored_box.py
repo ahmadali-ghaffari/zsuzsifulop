0d7f6ffcf8296421ebc6f3ee6ae42ae4d97e8f87
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-18 14:06:30 +0100

    style(colored_box.py): cleaner code

--- week-03/day-03/colored_box.py
+++ week-03/day-03/colored_box.py
@@ -1,11 +1,9 @@
+# draw a box that has different colored lines on each edge.
+
 from tkinter import *
-
 root = Tk()
-
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
-
-# draw a box that has different colored lines on each edge.
 left_line = canvas.create_line(10, 10, 10, 300, fill= "green")
 under_line = canvas.create_line(10, 300, 300, 300, fill ="orange")
 right_line = canvas.create_line(300, 10, 300, 300, fill="blue")
