394735f7c23ff10cb5542df3edf41e55932d863c
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-18 14:19:51 +0100

    style(line_play.py): cleaner code

--- week-03/day-03/line_play.py
+++ week-03/day-03/line_play.py
@@ -1,14 +1,14 @@
+# reproduce this:
+# [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
+
 from tkinter import *
 
 root = Tk()
-
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
 
-# reproduce this:
-# [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
 def line_play(a, b, c, d, color):
-    line = canvas.create_line(a, b, c, d, fill=color)
+    line = canvas.create_line(a, b, c, d, fill = color)
 
 i = 0
 while i < 300:
