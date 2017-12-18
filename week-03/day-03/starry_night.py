941fe70703a7ddeff018eeb889ada8ac7c50fe51
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-18 14:03:51 +0100

    style(starry_night.py): cleaner code

--- week-03/day-03/starry_night.py
+++ week-03/day-03/starry_night.py
@@ -1,16 +1,15 @@
-from tkinter import *
-import random
-
-root = Tk()
-
-canvas = Canvas(root, width='300', height='300')
-canvas.pack()
-
 # draw the night sky:
 # - The background should be black
 # - The stars should be small squares
 # - The stars should have random positions on the canvas
 # - The stars should have random color (some shade of grey)
+
+from tkinter import *
+import random
+
+root = Tk()
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
 background = canvas.create_rectangle(0, 0, 300, 300, fill="black")
 for i in range(0, 20):
     x_top = random.randint(0, 300)
