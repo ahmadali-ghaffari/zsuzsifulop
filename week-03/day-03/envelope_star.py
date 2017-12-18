d0827fe9e4b5cb34ad2b4dc593436a843c8a4a59
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-18 14:10:38 +0100

    style(envelope_star.py): cleaner code

--- week-03/day-03/envelope_star.py
+++ week-03/day-03/envelope_star.py
@@ -1,30 +1,30 @@
+# reproduce this:
+# [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/envelope-star/r2.png]
+
 from tkinter import *
-
 root = Tk()
-
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
 
-# reproduce this:
-# [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/envelope-star/r2.png]
 def line_play_green(a, b, c, d):
     line = canvas.create_line(a, b, c, d, fill="green")
 
 i = 0
 while i < 150:
     i += 10
-    line_play_green(i, 150, 150, 150-i)
+    line_play_green(i, 150, 150, 150 - i)
 i = 0
 while i < 150:
     i += 10
-    line_play_green(150+i, 150, 150, i)
+    line_play_green(150 + i, 150, 150, i)
 i = 0
 while i < 150:
     i += 10
-    line_play_green(i, 150, 150, 150+i)
+    line_play_green(i, 150, 150, 150 + i)
 i = 0
 while i < 150:
     i += 10
-    line_play_green(150+i, 150, 150, 300-i)
+    line_play_green(150 + i, 150, 150, 300 - i)
+
 
 root.mainloop()
