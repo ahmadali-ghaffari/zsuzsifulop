ab7bcec229d2ff6daba1ac48bb940394061894f7
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-20 13:59:23 +0100

    style(square_grid.py): cleaner code

--- week-03/day-05/square_grid.py
+++ week-03/day-05/square_grid.py
@@ -7,7 +7,7 @@ canvas = Canvas(root, width='600', height='600')
 canvas.pack()
 
 def mother_square(x, y, size, width):
-    r = lambda: random.randint(0,255)
+    r = lambda: random.randint(0, 255)
     random_color = '#%02X%02X%02X' % (r(),r(),r())
     time.sleep(0.1)
     canvas.update()
@@ -20,6 +20,6 @@ def mother_square(x, y, size, width):
         mother_square(x - size/4, y + 3*size/4, size/2, width - 2 )
         mother_square(x + 3*size/4, y + 3*size/4, size/2, width - 2)
 
-mother_square(150, 150, 300, 10)
 
+mother_square(150, 150, 300, 10)
 root.mainloop()
