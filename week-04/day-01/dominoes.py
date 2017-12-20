9ea389606f2403db38883a8d2f41f1f23acc314b
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-20 14:03:24 +0100

    style(dominoes.py): cleanercode

--- week-04/day-01/dominoes.py
+++ week-04/day-01/dominoes.py
@@ -10,13 +10,13 @@ def initialize_dominoes():
     dominoes.append(Domino(7, 1))
     return dominoes
 
-dominoes = initialize_dominoes()
 
+dominoes = initialize_dominoes()
 
 for i in range(len(dominoes)):
     for j in range(1,len(dominoes)):
         if dominoes[i].values[1] == dominoes[j].values[0]:
-            dominoes[i+1], dominoes[j] = dominoes[j], dominoes[i+1]
+            dominoes[i + 1], dominoes[j] = dominoes[j], dominoes[i + 1]
 print(dominoes)
 
 
