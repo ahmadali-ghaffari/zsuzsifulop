e9a98a1c52169d9dd774bdb3b68d21159b23a2d4
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-12-20 14:06:28 +0100

    fleet_of_things.py): cleaner code

--- week-04/day-01/fleet_of_things.py
+++ week-04/day-01/fleet_of_things.py
@@ -1,12 +1,13 @@
-from fleet import Fleet
-from thing import Thing
-
-fleet = Fleet()
 # Create a fleet of things to have this output:
 # 1. [ ] Get milk
 # 2. [ ] Remove the obstacles
 # 3. [x] Stand up
 # 4. [x] Eat lunch
+
+from fleet import Fleet
+from thing import Thing
+
+fleet = Fleet()
 
 item_1 = Thing("Get milk")
 print(item_1)
