d83db1daf7816f12b32b848486901a840a43aa2d
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2018-01-10 16:10:32 +0100

    fix(ajax.js): cleaner code

--- week-10/day-02/assets/ajax.js
+++ week-10/day-02/assets/ajax.js
@@ -1,17 +1,18 @@
-'use strict'
-function ajax(method, url, callback, reqBody){
+'use strict';
+
+function ajax(method, url, callback, reqBody) {
     let x = new XMLHttpRequest();
     x.open(method, url, true);
     x.setRequestHeader("Content-Type", "application/json");
-    x.onload = function(){
+    x.onload = function () {
         console.log({"callback": x.responseText});
         let data = JSON.parse(x.responseText);
         callback(data);
     };
     let postdata = null;
-    if (reqBody){
+    if (reqBody) {
         postdata = JSON.stringify(reqBody);
-    };
+    }
     console.log(postdata);
-    x.send(postdata);  
-};
+    x.send(postdata);
+}
